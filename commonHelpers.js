5a144adaa434b042ef3bfe52af521e8eefd6ee21
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2024-02-07 15:56:22 +0000

    Deploying to gh-pages from @ AndriiPavuk/goit-js-hw-12@2e3ba70d83d1f1b68aca5b06a84dedeba8b14eb1 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,6 @@
-import{S as p,i as g}from"./assets/vendor-5b791d57.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&r(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(e){if(e.ep)return;e.ep=!0;const s=n(e);fetch(e.href,s)}})();const m="https://pixabay.com/api/";function d(t,i){const n=new URLSearchParams({key:KEY_API,q:i,page:t,per_page:40,image_type:"photo",orientation:"horizontal"}),r=`${m}?${n.toString()}`;return fetch(r).then(e=>e.json())}function h(t){return t.map(({largeImageURL:i,webformatURL:n,tags:r,likes:e,views:s,comments:o,downloads:f})=>`<li class="gallery-item">
+import{S as v,i as u}from"./assets/vendor-5b791d57.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const l of s.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&r(l)}).observe(document,{childList:!0,subtree:!0});function n(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(t){if(t.ep)return;t.ep=!0;const s=n(t);fetch(t.href,s)}})();const b="25810966-6fb22a4db6c9a757ebd742847",L="https://pixabay.com/api/";function m(i,e){const n=new URLSearchParams({key:b,q:e,page:i,per_page:40,image_type:"photo",orientation:"horizontal"}),r=`${L}?${n.toString()}`;return fetch(r).then(t=>t.json())}function g(i){return i.map(({largeImageURL:e,webformatURL:n,tags:r,likes:t,views:s,comments:l,downloads:y})=>`<li class="gallery-item">
 
-              <a href="${i}"
+              <a href="${e}"
 
                   class="link-img">
 
@@ -24,7 +24,7 @@ import{S as p,i as g}from"./assets/vendor-5b791d57.js";(function(){const i=docum
 
                       <!-- <p class="list-info-name">Likes:</p> -->
 
-                      <p class="list-info-value">${e}</p>
+                      <p class="list-info-value">${t}</p>
 
                   </li>
 
@@ -52,7 +52,7 @@ import{S as p,i as g}from"./assets/vendor-5b791d57.js";(function(){const i=docum
 
                       <!-- <p class="list-info-name">Comments:</p> -->
 
-                      <p class="list-info-value">${o}</p>
+                      <p class="list-info-value">${l}</p>
 
                   </li>
 
@@ -66,11 +66,11 @@ import{S as p,i as g}from"./assets/vendor-5b791d57.js";(function(){const i=docum
 
                       <!-- <p class="list-info-name">Downloads:</p> -->
 
-                      <p class="list-info-value">${f}</p>
+                      <p class="list-info-value">${y}</p>
 
                   </li>
 
               </ul>
 
-          </li>`).join("")}const c=document.querySelector(".gallery"),a=document.querySelector(".form"),l=document.querySelector(".loader"),y=new p(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});a.addEventListener("submit",v);l.hidden=!0;a.dataset.page="1";function v(t){if(t.preventDefault(),t.currentTarget.elements.query.value.trim()===""){u("Enter anything in the search field!");return}b()}function b(){l.hidden=!1,c.innerHTML="",a.dataset.page="1",L()}function u(t){g.error({position:"topRight",messageColor:"brown",message:t,timeout:3e3})}function L(){const t=parseInt(a.dataset.page,10);d(t,a.elements.query.value.trim()).then(i=>{S(i)}).catch(i=>console.error(i)).finally(()=>{l.hidden=!0})}function S(t){if(t.hits.length===0){u("Sorry, there are no images matching your search query. Please try again!");return}c.insertAdjacentHTML("beforeend",h(t.hits)),y.refresh()}
+          </li>`).join("")}const a=document.querySelector(".gallery"),o=document.querySelector(".more"),h=document.querySelector(".form"),d=document.querySelector(".loader");h.addEventListener("submit",S);o.addEventListener("click",w);d.hidden=!0;const p=new v(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});let f="",c=0;function S(i){if(i.preventDefault(),i.currentTarget.elements.query.value.trim()===""){u.error({position:"topRight",messageColor:"brown",message:"Enter anything in the search field!",timeout:3e3});return}f=i.currentTarget.elements.query.value,c=1,o.disabled=!1,o.textContent="More",o.hidden=!0,d.hidden=!1,a.innerHTML="",m(c,f).then(e=>{if(e.hits.length===0){u.error({position:"topRight",messageColor:"brown",message:"Sorry, there are no images matching your search query. Please try again!",timeout:3e3});return}c+=1,a.insertAdjacentHTML("beforeend",g(e.hits)),p.refresh(),e.hits.length===40&&(o.hidden=!1),a.lastElementChild.scrollIntoView({behavior:"smooth",block:"start"})}).catch(e=>console.log(e)).finally(()=>{d.hidden=!0}),h.reset()}function w(i){i.preventDefault(),o.hidden=!0,d.hidden=!1,m(c,f).then(e=>{c+=1,a.insertAdjacentHTML("beforeend",g(e.hits)),p.refresh(),e.hits.length===40&&(o.hidden=!1),e.hits.length<40&&(o.hidden=!1,o.disabled=!0,o.textContent="Images are over"),a.lastElementChild.scrollIntoView({behavior:"smooth",block:"start"})}).catch(e=>console.log(e)).finally(()=>{d.hidden=!0})}
 //# sourceMappingURL=commonHelpers.js.map
